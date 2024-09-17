bf876541a63d3b89004301e5b806c218f59f5eb3
interspeech <harm0104@gmail.com>
2024-09-17 12:23:13 +0000

    update workable version

--- resample.py
+++ resample.py
@@ -5,11 +5,10 @@ import glob
 
 def resample(path, target_sr=16000, output_path=None):
     y, sr = sf.read(path)
-    y = librosa.resample(y, orig_sr=22050, target_sr=target_sr)
+    y = librosa.resample(y, orig_sr=48000, target_sr=target_sr)
     if output_path:
         sf.write(output_path, y, target_sr)
     else:
         sf.write(path, y, target_sr)
 
-for item in Path('finetune/wavs').glob("*.wav"):
-    resample(item)
+resample('test_audio/joe_example.wav', target_sr=16000, output_path='test_audio/16000_joe_example_compare.wav')
